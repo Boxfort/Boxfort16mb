600388484319b513dd93f5f7f6d7aabaa036299a
Jack Anderson <jack_120@hotmail.co.uk>
2016-06-21 17:07:46 +0100

    very basic html

--- index.php
+++ index.php
@@ -6,6 +6,25 @@
     <title>Boxfort16mb</title>
   </head>
   <body>
-    <h1>The Title!</h1>
+    <header>
+      <h1 id="logo">Logo</h1>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="downloads.php">Downloads</a></li>
+          <li><a href="forum.php">Forum</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        <ul>
+      </nav>
+    </header>
+    <div id="container">
+      <aside>
+        <h2>Login</h2>
+        <form method="post">
+          <label>Username</label><input id="username" name="username"></input><br/>
+          <label>Password</label><input id="password" name="password" type="password"></input>
+        </form>
+      </aside>
+    </div>
   </body>
 </html>
