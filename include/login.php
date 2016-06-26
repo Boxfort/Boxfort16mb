a9574a32657ce790de241cdbe6d1bba4b355eb71
Jack Anderson <jack_120@hotmail.co.uk>
2016-06-26 15:39:56 +0100

    register form layout

--- include/login.php
+++ include/login.php
@@ -8,9 +8,9 @@
     }
   ?>
   <label for="username" class="sr-only">Username</label>
-  <input id="username" class="form-control" name="username" placeholder="Username" required autofocus></input>
+  <input id="username" class="form-control aside" name="username" placeholder="Username" required autofocus></input>
   <label for="password" class="sr-only">Password</label>
-  <input id="password" class="form-control" name="password" type="password" placeholder="Password" required></input>
+  <input id="password" class="form-control aside" name="password" type="password" placeholder="Password" required></input>
   </br>
   <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
 </form>
--- register.php
+++ register.php
@@ -2,6 +2,48 @@
   include 'include/header.php';
 ?>
 
+<h1>Register a new account</h1>
+<form action="###" method="post">
+  <div class="table-responsive">
+    <table class="table">
+      <tbody>
+        <tr>
+          <td><label class="required">Username</label></td>
+          <td><input class="form-control register" placeholder="Username"></td>
+        <tr>
+        <tr>
+          <td><label class="required">First name</label></td>
+          <td><input class="form-control register" placeholder="First name"></td>
+        </tr>
+        <tr>
+          <td><label>Last name</label></td>
+          <td><input class="form-control register" placeholder="Last name"></td>
+        </tr>
+        <tr>
+          <td><label class="required">E-mail</label></td>
+          <td><input class="form-control register" placeholder="example@website.com"></td>
+        </tr>
+        <tr>
+          <td><label class="required">Confirm e-mail</label></td>
+          <td><input class="form-control register" placeholder="example@website.com"></td>
+        </tr>
+        <tr>
+          <td><label class="required">Password</label></td>
+          <td><input class="form-control register" placeholder="Password"></td>
+        </tr>
+        <tr>
+          <td><label class="required">Confirm password</label></td>
+          <td><input class="form-control register" placeholder="Password"></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><button class="btn btn-primary btn-register">Register</button></td>
+        </tr>
+      <tbody>
+    </table>
+  </div>
+</form>
+
 <?php
   include 'include/footer.php';
 ?>
